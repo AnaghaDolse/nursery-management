30c76714f14e7f044da1c8e8576cf12091d0176c
AnaghaDolse <anaghadolase08@gmail.com>
2026-06-26 22:15:26 +0530

    Implemented favorites page with plant card integration - added favorites page, connected saved favorites from localStorage, reused PlantCard component, and improved favorites rendering flow

--- frontend/src/pages/Favourites.js
+++ frontend/src/pages/Favourites.js
@@ -1,30 +1,38 @@
 import { useSelector } from 'react-redux'
+import PlantCard from '../components/PlantCard'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const Favourites = () => {
+  const navigate = useNavigate()
   const { data } = useSelector((state) => state.plants)
 
-  const faovuriteIds = JSON.parse(localStorage.getItem('favourites')) || []
+  const faovuriteIds = JSON.parse(localStorage.getItem('favorites')) || []
 
   const favouritePlants = data.filter((plant) =>
     faovuriteIds.includes(plant._id),
   )
+  // ✅ Add condition here
+  if (favouritePlants.length === 0) {
+    return (
+      <div>
+        <h2>My Favourites ❤️</h2>
+        <p>No favourite plants yet.</p>
+        <p>Click the ❤️ button on a plant to add it here.</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>My Favourites</h2>
 
-      {favouritePlants.length === 0 ? (
-        <div>
-          <h3>No Favorite Yet</h3>
-          <p>Start exploring and save plants you love.</p>
-        </div>
-      ) : (
-        favouritePlants.map((plant) => (
-          <div key={plant._id}>
-            <h3>{plant.name}</h3>
-            <p>₹{plant.price}</p>
-          </div>
-        ))
-      )}
+      <button onClick={() => navigate('/add-plant')}>← Back to plant</button>
+
+      <div className='plant-grid'>
+        {favouritePlants.map((plant) => (
+          <PlantCard key={plant._id} plant={plant} />
+        ))}
+      </div>
     </div>
   )
 }
