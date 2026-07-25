179d9a9270e6f37208ea74814ce1f50028ea0865
AnaghaDolse <anaghadolase08@gmail.com>
2026-07-25 19:38:06 +0530

    feat(auth): add email utility and configure secure environment setup

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,21 @@
+import { text } from 'express'
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+})
+
+export const sendEmail = async (options) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  }
+
+  await transporter.sendMail(mailOptions)
+}
